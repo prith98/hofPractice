d8a06447ee8aedc1fe4c17ae62d31e4d99288ae1
Prith <prith98@gmail.com>
2021-10-20 11:49:00 -0700

    first update

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use 
-// the underscore functions within them. 
+// Here we'll be writing new functions, but these functions will use
+// the underscore functions within them.
 
 /*
  *
@@ -23,6 +23,16 @@ var moreFruits = function(fruits) {
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
 
+  var multipleFive = 0;
+
+  _.each(numbers, function(number) {
+    if (number % 5 === 0) {
+      multipleFive += 1;
+    }
+  })
+
+  return multipleFive;
+
 };
 
 /*
@@ -34,16 +44,40 @@ var multiplesOfFive = function(numbers) {
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function(fruits, targetFruit) {
 
+  var oneFruit = _.filter(fruits, function(fruit) {
+    if (fruit === targetFruit) {
+      return fruit;
+    }
+  })
+
+  return oneFruit;
+
 };
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
 
+  //console.log(letter);
+  var fruitsThatStartWith = _.filter(fruits, function(fruit, index) {
+    //console.log(index);
+    //console.log(fruit);
+    /*if (fruit[0] === letter) {
+      return index;
+    }*/
+    return fruit[0] === letter;
+  })
+  //console.log(fruitsThatStartWith);
+  return fruitsThatStartWith;
+
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function(desserts) {
+
+  return _.filter(desserts, function(recipe) {
+    return recipe['type'] === 'cookie';
+  })
 
 };
 
@@ -55,7 +89,15 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-  
+
+  return _.reduce(products, function(sum, product) {
+
+    //console.log(price);
+    var numPrice = Number(product.price.substring(1));
+    return sum + numPrice;
+
+  }, 0)
+
 };
 
 // return an object consisting of dessert types and how many of each.
@@ -68,7 +110,7 @@ var dessertCategories = function(desserts) {
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
-  
+
 };
 
 // return an boolean stating if there exists a movie with a shorter
@@ -102,7 +144,7 @@ var glutenFree = function(desserts) {
 //
 // having trouble with decimals? check out this article:
 // http://adripofjavascript.com/blog/drips/avoiding-problems-with-decimal-math-in-javascript.html
-// 
+//
 /*
 
  example output:
@@ -111,7 +153,7 @@ var glutenFree = function(desserts) {
     {
       id: 1,
       product: 'Olive Oil',
-      price: '$12.1', 
+      price: '$12.1',
       salePrice: '$9.61'
     }
   ];
